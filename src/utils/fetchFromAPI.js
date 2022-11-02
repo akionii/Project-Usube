ac87a70dc57a7a8e80dc6c372a70e05dd0f82d51
biskwt <akioni.uwu@gmail.com>
2022-11-02 21:52:41 +0100

    Repaired videos from RapidAPI not appearing.

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,12 +4,15 @@ export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   params: {
-    maxResults: 50,
+    relatedToVideoId: '7ghhRHRP6t4',
+    part: 'id,snippet',
+    type: 'video',
+    maxResults: '50'
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-  },
+    'X-RapidAPI-Key': 'a415232396msh1ca9d4f6630cb3ep18528cjsn2453398e0ac7',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
 };
 
 export const fetchFromAPI = async (url) => {
