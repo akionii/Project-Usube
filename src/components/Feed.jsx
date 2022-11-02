67dea5059cc01145cdf53fffa0dbbe6a13b4bae6
biskwt <akioni.uwu@gmail.com>
2022-11-02 19:53:52 +0100

    Quick Update & Warning repaied 'index.css' line:65

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -20,7 +20,7 @@ const Feed = () => {
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#444", }}>
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2022 biskwt
         </Typography>
       </Box>
